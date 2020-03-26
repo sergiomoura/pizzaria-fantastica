af507a8799aa9f1192a0c85b98dc386fff0e92ef
Sérgio Moura <smouracalmon@gmail.com>
2020-03-26 00:21:48 -0300

    feat: adicionada página de detalhes da pizza

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -11,9 +11,24 @@ module.exports = {
 			pizza => pizza.id == req.params.id
 		);
 
+		// Capturando a posição da pizza no array
+		let pos = pizzas.indexOf(pizza);
+
+		// determinando o id da próxima pizza e da anterior
+		let idPrev = null;
+		let idNext = null;
+
+		if(pos > 0){
+			idPrev = pizzas[pos -1].id;
+		}
+
+		if(pos < pizzas.length - 1){
+			idNext = pizzas[pos + 1].id;
+		}
+
 		// Retornando a pizza para o usuário
 		if(pizza){
-			res.render("pizza",{pizza});
+			res.render("pizza",{pizza, idNext, idPrev});
 		} else {
 			res.render("erros/pizzaNaoEncontrada",{id:req.params.id});
 		}
