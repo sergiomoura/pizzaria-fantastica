f183a5f8965121cf3f379c193a45ce3baabd6315
Sérgio Moura <smouracalmon@gmail.com>
2020-04-07 02:47:18 -0300

    feat: criando middleware que verifica usuario logado e protejendo rotas

---
+++ middlewares/VerificaUsuarioLogado.js
@@ -0,0 +1,7 @@
+module.exports = (req, res, next) => {
+    if(req.session.user){
+        next();
+    } else {
+        res.redirect("/adm/login");
+    }
+}
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const VerificaUsuarioLogado = require("../middlewares/VerificaUsuarioLogado");
 const upload = require("../lib/upload");
 
 const PizzasController = require('../controllers/PizzasController');
@@ -8,12 +8,12 @@ const PizzasController = require('../controllers/PizzasController');
 /* GET home page. */
 router.get('/', PizzasController.index);
 router.get('/busca', PizzasController.search);
-router.get('/pizzas/:id/edit', PizzasController.edit);
-router.get('/pizzas/create', PizzasController.create);
+router.get('/pizzas/:id/edit', VerificaUsuarioLogado,PizzasController.edit);
+router.get('/pizzas/create', VerificaUsuarioLogado, PizzasController.create);
 router.get('/pizzas/:id', PizzasController.show);
-router.get('/pizzas', PizzasController.list);
-router.post('/pizzas', upload.single("img"), PizzasController.store);
-router.put('/pizzas/:id/update', PizzasController.update);
-router.delete('/pizzas/:id', PizzasController.delete);
+router.get('/pizzas', VerificaUsuarioLogado, PizzasController.list);
+router.post('/pizzas', VerificaUsuarioLogado, upload.single("img"), PizzasController.store);
+router.put('/pizzas/:id/update', VerificaUsuarioLogado, PizzasController.update);
+router.delete('/pizzas/:id', VerificaUsuarioLogado, PizzasController.delete);
 
 module.exports = router;
