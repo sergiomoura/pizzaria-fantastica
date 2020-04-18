b10f93247d94cea140e6097697dfdc8249720a5a
Sérgio Moura <smouracalmon@gmail.com>
2020-04-18 01:47:13 -0300

    feat: form de criação de usuários

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,6 +3,9 @@ const users = require("../database/Users.json");
 const UsersController = {
     list: (req, res) => {
         res.render('crud-usuarios/list',{users});
+    },
+    create: (req,res) =>{
+        res.render('crud-usuarios/create');
     }
 }
 
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
 const UsersController = require('../controllers/UsersController');
 
 /* GET home page. */
+router.get('/users/create', UsersController.create);
 router.get('/users', UsersController.list);
 
 module.exports = router;
