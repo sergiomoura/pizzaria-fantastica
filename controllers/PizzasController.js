5926d699485006cb45dfc63477b52087be609e19
Sérgio Moura <smouracalmon@gmail.com>
2021-07-28 16:17:08 -0300

    Adicionada pagina que exibe uma pizza

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,9 @@
 module.exports = {
 	index: (req, res)=>{
 		res.render("index");
+	},
+
+	show: (req,res) => {
+		res.render("pizza");
 	}
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -6,5 +6,6 @@ const PizzasController = require('../controllers/PizzasController');
 
 /* GET home page. */
 router.get('/', PizzasController.index);
+router.get('/pizzas/:id', PizzasController.show);
 
 module.exports = router;
