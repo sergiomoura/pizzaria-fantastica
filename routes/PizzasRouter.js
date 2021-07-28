74b93a9fff4fbd4ad8430c9d7faaba78f2557097
Sérgio Moura <smouracalmon@gmail.com>
2021-07-28 15:52:53 -0300

    Adequando rotas para inicio de pratica

--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -7,7 +7,4 @@ const PizzasController = require('../controllers/PizzasController');
 /* GET home page. */
 router.get('/', PizzasController.index);
 
-router.get('/pizzas/:id',PizzasController.show);
-
-
 module.exports = router;
