f457a620db0e85809fd38f1b6db44c6148be109c
Camila Muzzi <camilamuzzicosta@gmail.com>
2021-07-28 22:11:03 -0300

    alterado o fluxo do mvc para pagina index e pizza filtro

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,16 @@
+const pizzasJson = require('../database/Pizzas.json');
+
 module.exports = {
 	index: (req, res)=>{
-		res.render("index");
+		res.render("index", {pizzasJson});
+	},
+	show: (req, res) => {
+		const { id } = req.params
+		let resultado = pizzasJson.filter(function (pizzas) {
+            if (pizzas.id == id)
+		        res.render("pizza", {pizzas})		
+		})
+		
 	}
+
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 const PizzasController = require('../controllers/PizzasController');
 
+
 /* GET home page. */
 router.get('/', PizzasController.index);
+router.get('/pizzas/:id', PizzasController.show);
 
 module.exports = router;
