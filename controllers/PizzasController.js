a97f92900905db3aafa1f980fd047bedadc7b0a6
Sérgio Moura <smouracalmon@gmail.com>
2021-07-28 21:53:14 -0300

    Mostrando as pizzas dinamicamente e exbindo a view de uma pizza (estatica)

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,5 +1,15 @@
+const pizzas = require('../database/Pizzas.json');
+
 module.exports = {
 	index: (req, res)=>{
-		res.render("index");
+		res.render("index", {pizzas});
+	},
+	show: (req,res) => {
+		// res.send("salve!!!  " + req.params.id);
+		
+		// 1 - Carregar a pizza de id passado (pizzas.find);
+
+		// 2 - Renderizar a view pizza passando a pizza encontrada
+		res.render("pizza")
 	}
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,6 @@ const PizzasController = require('../controllers/PizzasController');
 
 /* GET home page. */
 router.get('/', PizzasController.index);
+router.get('/pizzas/:id', PizzasController.show);
 
 module.exports = router;
