7076ba39ed1243edb10a1de7e0a8f282c10b7712
Camila Muzzi <camilamuzzicosta@gmail.com>
2021-08-11 15:02:51 -0300

    aula de rotas MVC

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -1,16 +1,61 @@
 const pizzasJson = require('../database/Pizzas.json');
 
+
 module.exports = {
 	index: (req, res)=>{
 		res.render("index", {pizzasJson});
 	},
 	show: (req, res) => {
 		const { id } = req.params
-		let resultado = pizzasJson.filter(function (pizzas) {
-            if (pizzas.id == id)
-		        res.render("pizza", {pizzas})		
-		})
-		
+		let proximo = id;
+		let voltar = id;
+		// 1 - Carregar a pizza de id passado (pizzas.find);		
+		let pizza = pizzasJson.find(pizza => pizza.id == id)
+		// 2 - Renderizar a view pizza passando a pizza encontrada
+		// {pizza:pizza}
+
+		if (id >= pizzasJson.length) {
+			//proximo =false
+			proximo = 1
+		} else {
+			proximo = parseInt(id) + 1
+		}
+		if (id <= 1) {
+			//voltar =false
+			voltar = pizzasJson.length
+		} else {
+			voltar = parseInt(id) - 1
+		}
+		res.render("pizza", { pizza, proximo: proximo, voltar })
+	},
+	busca:(req, res) =>
+	{		
+		const  busca  = req.query.busca;
+        let pizzaEncontrada = pizzasJson.filter(pizza => pizza.nome.toUpperCase().includes(busca.toUpperCase()))
+		res.render("index", {pizzasJson: pizzaEncontrada});
+	},
+	create:(req, res) =>
+	{
+		res.render("pizza-create")
+	},
+	store:(req, res) =>
+	{
+		let pizza = {
+			id:pizzasJson[pizzasJson.length -1].id + 1,
+			nome: req.body.nome,
+			ingredientes: req.body.ingredientes.split(","),
+			preco: Number(req.body.preco),
+			img:"/img/" + req.file.filename,
+			destaque:true
+		}
+		pizzasJson.push(pizza);
+		res.render("index", {pizzasJson})
 	}
+
+
+// req.query.busca metodo get
+// req.query.body metodo post
+
+//X.INCLUDES
 
 }
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+
 
 const PizzasController = require('../controllers/PizzasController');
+
+const storage = multer.diskStorage({
+    destination: (req, file,cb)=>{
+        cb(null, './public/img')
+    },
+    filename:(req, file, cb) =>{
+        cb(null, file.originalname)
+    }
+})
+const uploadFile = multer({storage:storage})
+
 
 
 /* GET home page. */
 router.get('/', PizzasController.index);
+router.get('/pizzas/create', PizzasController.create);
+router.post('/pizzas/create', uploadFile.single('img'), PizzasController.store);
 router.get('/pizzas/:id', PizzasController.show);
+router.get('/busca', PizzasController.busca);
+
+
 
 module.exports = router;
